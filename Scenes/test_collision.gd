ddda821328db20f3034dce635954db4ab8dd38b1
Nathan Ostrander <125089113+Proxy711@users.noreply.github.com>
2025-02-12 16:46:05 -0500

    Enhanced Movement
    Made a jump mechanic and adjusted spikes to account for it.

--- Scenes/test_collision.gd
+++ Scenes/test_collision.gd
@@ -1,10 +1,18 @@
-extends CharacterBody2D
+extends Area2D
+
+var playerDied: bool = false # Not being used right now.
+
+func _physics_process(delta: float) -> void:
+	for body in get_overlapping_bodies():
+		if body.is_in_group("player") and not body.invulnerable:
+			print("Player died") # For testing. Will spam console.
+			# playerDied = true  I suspect we may need this later.
+			pass
+		else:
+			print("Player is invulnerable!") # For testing. Will spam console.
+			pass # Nothing happens if they're invulnerable.
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass #Just skips this function for now (Can update if we need to)
-
-func _on_area_2d_body_entered(body: Node2D) -> void: #Signal that determines if a body enters the 2d area
-	if body.is_in_group("player"): # Chcecks to see if the body that was entered is in the group player
-		print("Player died") #Update later
+# The for loop is to continually check whether or not there's a body inside the collision. 
+# The original trap code checked if a player had walked into it, but now that they can over jump it needs to check instead
+# if the player has walked into it, or if they mistimed their jump. 
