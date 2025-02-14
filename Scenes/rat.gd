20a2ec8b8f06ab2bc0eb54b5aa41be494a590c46
Amo39 <Santos4735@gmail.com>
2025-02-13 22:47:28 -0500

    Rat Basic Movement
    Added Just a basic movement that walks in a square

--- Scenes/rat.gd
+++ Scenes/rat.gd
@@ -1,1 +1,48 @@
-extends Node
+extends CharacterBody2D
+
+const SPEED = 5 #Movement Speed (Change to fit character movement)
+@onready var animated_sprite = $AnimatedSprite2D
+@export var sizeOfPath: float = 10.0
+
+var direction = -1
+var time: float = 0.0
+var startingPosition: Vector2
+
+func _ready() -> void:
+	startingPosition  = position
+	time += SPEED+ (randf() * 100)
+	
+func _process(delta: float) -> void:
+	
+	time += delta * SPEED 
+
+	# Square Movement
+	var x = 0.0
+	var y = 0.0
+	
+	var phase = int(time / sizeOfPath) % 4
+	var progress = fposmod(time, sizeOfPath)
+	
+	if phase == 0:
+		#Move Right
+		x = progress
+		y = 0
+		animated_sprite.play("RightMovement")
+	elif phase == 1:
+		#Move Down
+		x = sizeOfPath
+		y = progress
+	elif phase == 2:
+		#Move left
+		x = sizeOfPath - progress
+		y = sizeOfPath
+		animated_sprite.play("LeftMovement")
+	elif phase == 3:
+		#Move up
+		x = 0
+		y = sizeOfPath - progress
+		animated_sprite.play("UpwardMovement")
+
+	
+
+	position = startingPosition + Vector2(x, y) 
