908250bcd1c0a90fc07a0c8e982603f678aef2e1
Monkey-Matthew <157081103+Monkey-Matthew@users.noreply.github.com>
2025-02-23 08:40:05 -0500

    Revert "Revert "Fixed Spam Jumping Closes #34""

--- Scripts/test_collision.gd
+++ Scripts/test_collision.gd
@@ -20,7 +20,7 @@ func _physics_process(delta: float) -> void:
 """
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.name == "Player" and not body.invulnerable and body.can_Jump:
+	if body.name == "Player" and not body.invulnerable:
 		Health.player_health -= 1 #Change later
 		# playerDied = true  I suspect we may need this later.
 		pass
