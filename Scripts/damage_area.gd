8a01b518d0de63f91a249406211689aa85b606cc
Matthew-Gonzales <monkeymatthew0217@gmail.com>
2025-03-18 10:20:51 -0400

    Added Constant Damage Closes #83
    I added it so that it takes damage when the player is inside and not just for the initial collision. Now I need to add a script so that the player has a damage cooldown for anything.

--- Scripts/damage_area.gd
+++ Scripts/damage_area.gd
@@ -1,12 +1,21 @@
 extends Area2D
 
 @export var damageAmount: float
+var playerInside: bool = false
 
+func _process(delta: float) -> void:
+	if playerInside == true:
+		Health.player_health -= damageAmount
+		
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Player" and not body.invulnerable:
-		Health.player_health -= damageAmount #Change later
+		playerInside = true
 		# playerDied = true  I suspect we may need this later.
 		pass
 	else:
 		#print("Player is invulnerable!") # For testing. Will spam console.
 		pass # Nothing happens if they're invulnerable.
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		playerInside = false
