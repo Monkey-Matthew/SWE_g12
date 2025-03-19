a316cbb847f5cf1b0a82b38e71f319ea0015d398
Matthew-Gonzales <monkeymatthew0217@gmail.com>
2025-03-19 12:34:52 -0400

    Created Enemy Drops Closes #89
    Instead of coins being on the ground. When the player kills an enemy it drops a coin at its' location. I also got rid of the coins that were normally in the game scene.

--- Scripts/enemy_health.gd
+++ Scripts/enemy_health.gd
@@ -4,11 +4,12 @@ extends Node
 var current_health: int #Current health the enemy has
 var parent_node: Node = get_parent()  #Reference to the parent node (CharacterBody2D)
 @onready var sprite: AnimatedSprite2D = get_parent().get_node("AnimatedSprite2D") #Reference to animated sprite
+var coin_scene = preload("res://Scenes/coin.tscn")  #Reference to coin scene
 
 func _ready():
 	current_health = max_health  #Initialize health when spawned
 	parent_node = get_parent()  #References parent node
-
+	
 func take_damage(amount: int):
 	current_health -= amount #Enemy loses health depending on the amount of damage that was called into the function
 	print(name, " took ", amount, " damage. Health left: ", current_health) #Debug
@@ -37,4 +38,16 @@ func tint_sprite_red(): #Tints the enemy sprites if they get hit
 
 func die(): #When the enemy's health is at 0 or less this function deletes the enemy from the scene.
 	print(parent_node.name, " defeated!")
+	coin_spawn(parent_node.position)
 	parent_node.queue_free()
+	
+
+func coin_spawn(position: Vector2):
+	var instance = coin_scene.instantiate()  #Creates an instance of the scene
+	instance.position = position
+	var game_scene = get_tree().get_root()
+	if game_scene:
+		game_scene.add_child(instance)  # Add coin to GameScene
+		print("Should spawn")
+	else:
+		print("GameScene not found!")
