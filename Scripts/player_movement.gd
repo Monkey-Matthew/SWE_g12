d6c565526b4bfbcc25eacf1b10c1ea3a8a7a4a55
Amo39 <Santos4735@gmail.com>
2025-04-09 12:18:12 -0400

    updated knockback
    will now knockback when moving to the enemy

--- Scripts/player_movement.gd
+++ Scripts/player_movement.gd
@@ -37,91 +37,28 @@ var projectile_path = preload("res://Scenes//star_projectile.tscn") #Preloads th
 func _physics_process(delta: float) -> void:
 	if not pause_script.paused: #Checks to see if the game is paused (if it is no movement can occur or changing direction
 		#Update movement direction
-		character_direction.x = Input.get_axis("left", "right")  # -1 for "left", 1 for "right" Gets input from left and right input
-		character_direction.y = Input.get_axis("up", "down")  # -1 for "up", 1 for "down" Gets input from up and down input
-		character_direction = character_direction.normalized()  # Normalize the direction for consistent speed
-																# (Used to stop diagonal movement from going twice as fast)
-		#Checks to see if jump is true
-		if can_Jump:
-			character_jump() #Jump function.
-		#Only updates velocity if there's input
-		if character_direction != Vector2.ZERO:
-			velocity = character_direction * movement_speed # Multiplies the movement speed by the direction to dtermine the velocity
-		
-		#Plays the appropriate animation based on direction
-			if character_direction.x < 0 and character_direction.y > 0: #Down and left (Movement)
-				if(rand_direction <= 0.5):
-					if animated_sprite.animation != "LeftRun": #If the animation isnt already playing then play animation
-						animated_sprite.play("LeftRun")
-						direction_facing = "Left" #Last direction facing 
-				else:
-					if animated_sprite.animation != "DownRun":
-						animated_sprite.play("DownRun")
-						direction_facing = "Down"
-			elif character_direction.x < 0 and character_direction.y < 0:  #Up and left (Movmement)
-				if(rand_direction <= 0.5):
-					if animated_sprite.animation != "LeftRun":
-						animated_sprite.play("LeftRun")
-						direction_facing = "Left"
-				else:
-					if animated_sprite.animation != "UpRun":
-						animated_sprite.play("UpRun")
-						direction_facing = "Up"
-			elif character_direction.x > 0 and character_direction.y > 0:  #Down and right (Movement)
-				if(rand_direction <= 0.5):
-					if animated_sprite.animation != "RightRun":
-						animated_sprite.play("RightRun")
-						direction_facing = "Right"
-				else:
-					if animated_sprite.animation != "DownRun":
-						animated_sprite.play("DownRun")
-						direction_facing = "Down"
-			elif character_direction.x > 0 and character_direction.y < 0:  #Up and right (Movement)
-				if(rand_direction <= 0.5):
-					if animated_sprite.animation != "RightRun":
-						animated_sprite.play("RightRun")
-						direction_facing = "Right"
-				else:
-					if animated_sprite.animation != "UpRun":
-						animated_sprite.play("UpRun")
-						direction_facing = "Up"
-			elif character_direction.y > 0:  #Down (Movement)
-				if animated_sprite.animation != "DownRun":
-					animated_sprite.play("DownRun")
-					direction_facing = "Down"
-			elif character_direction.y < 0:  #Up (Movement)
-				if animated_sprite.animation != "UpRun":
-					animated_sprite.play("UpRun")
-					direction_facing = "Up"
-			elif character_direction.x > 0:  #Right (Movement)
-				if animated_sprite.animation != "RightRun":
-					animated_sprite.play("RightRun")
-					direction_facing = "Right"
-			elif character_direction.x < 0:  #Left (Movement)
-				if animated_sprite.animation != "LeftRun":
-					animated_sprite.play("LeftRun")
-					direction_facing = "Left"
-		else:
-			#Stops the movement gradually
-			velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
-
+		var input_velocity = false
+		if not is_knocked_back:
+			character_direction.x = Input.get_axis("left", "right")  # -1 for "left", 1 for "right" Gets input from left and right input
+			character_direction.y = Input.get_axis("up", "down")  # -1 for "up", 1 for "down" Gets input from up and down input
+			character_direction = character_direction.normalized()  # Normalize the direction for consistent speed
+																	# (Used to stop diagonal movement from going twice as fast)
+			#Checks to see if jump is true
+			if can_Jump:
+				character_jump() #Jump function.
+			#Only updates velocity if there's input
+			if character_direction != Vector2.ZERO:
+				velocity = character_direction * movement_speed # Multiplies the movement speed by the direction to dtermine the velocity
+				movement_animation(character_direction)
+				input_velocity = true
+			else:
+				#Stops the movement gradually
+				velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 	#When there is no movement, switch to idle animation depending on the direction the player was last facing
 	if velocity == Vector2.ZERO:
-		if direction_facing == "Down": #Down idle animation
-			if animated_sprite.animation != "DownIdle": 
-				animated_sprite.play("DownIdle")
-		if direction_facing == "Up": #Up idle animation
-			if animated_sprite.animation != "UpIdle":
-				animated_sprite.play("UpIdle")
-		if direction_facing == "Left": #Left idle animation
-			if animated_sprite.animation != "LeftIdle":
-				animated_sprite.play("LeftIdle")
-		if direction_facing == "Right": #Right idle animation
-			if animated_sprite.animation != "RightIdle":
-				animated_sprite.play("RightIdle")
-	
-	move_and_slide() #Move the character based on the velocity variable above
+		idle_animation()
 
+	move_and_slide() #Move the character based on the velocity variable above
 	if(Health.player_health <= 0): 	#Checks to see if character died
 		Health.player_health = 0.01 #Fixes glitch that causes the player not to die
 		Reload.start() #Restarts the scene as of now
@@ -212,3 +149,76 @@ func apply_knockback(direction: Vector2, force: float): #Make sure to knockback
 	velocity = direction * actual_force #makes the velocity for the knockback
 	knockback_timer.start()
 	print ("Player knockback ", actual_force)
+
+
+func idle_animation():
+	if direction_facing == "Down": #Down idle animation
+		if animated_sprite.animation != "DownIdle": 
+			animated_sprite.play("DownIdle")
+	if direction_facing == "Up": #Up idle animation
+		if animated_sprite.animation != "UpIdle":
+			animated_sprite.play("UpIdle")
+	if direction_facing == "Left": #Left idle animation
+		if animated_sprite.animation != "LeftIdle":
+			animated_sprite.play("LeftIdle")
+	if direction_facing == "Right": #Right idle animation
+		if animated_sprite.animation != "RightIdle":
+			animated_sprite.play("RightIdle")
+
+func movement_animation(p_char_dir: Vector2)-> void:
+		if character_direction.x < 0 and character_direction.y > 0: #Down and left (Movement)
+			if(rand_direction <= 0.5):
+				if animated_sprite.animation != "LeftRun": #If the animation isnt already playing then play animation
+					animated_sprite.play("LeftRun")
+					direction_facing = "Left" #Last direction facing 
+			else:
+				if animated_sprite.animation != "DownRun":
+					animated_sprite.play("DownRun")
+					direction_facing = "Down"
+		elif character_direction.x < 0 and character_direction.y < 0:  #Up and left (Movmement)
+			if(rand_direction <= 0.5):
+				if animated_sprite.animation != "LeftRun":
+					animated_sprite.play("LeftRun")
+					direction_facing = "Left"
+			else:
+				if animated_sprite.animation != "UpRun":
+					animated_sprite.play("UpRun")
+					direction_facing = "Up"
+		elif character_direction.x > 0 and character_direction.y > 0:  #Down and right (Movement)
+			if(rand_direction <= 0.5):
+				if animated_sprite.animation != "RightRun":
+					animated_sprite.play("RightRun")
+					direction_facing = "Right"
+			else:
+				if animated_sprite.animation != "DownRun":
+					animated_sprite.play("DownRun")
+					direction_facing = "Down"
+		elif character_direction.x > 0 and character_direction.y < 0:  #Up and right (Movement)
+			if(rand_direction <= 0.5):
+				if animated_sprite.animation != "RightRun":
+					animated_sprite.play("RightRun")
+					direction_facing = "Right"
+			else:
+				if animated_sprite.animation != "UpRun":
+					animated_sprite.play("UpRun")
+					direction_facing = "Up"
+		elif character_direction.y > 0:  #Down (Movement)
+			if animated_sprite.animation != "DownRun":
+				animated_sprite.play("DownRun")
+				direction_facing = "Down"
+		elif character_direction.y < 0:  #Up (Movement)
+			if animated_sprite.animation != "UpRun":
+				animated_sprite.play("UpRun")
+				direction_facing = "Up"
+		elif character_direction.x > 0:  #Right (Movement)
+			if animated_sprite.animation != "RightRun":
+				animated_sprite.play("RightRun")
+				direction_facing = "Right"
+		elif character_direction.x < 0:  #Left (Movement)
+			if animated_sprite.animation != "LeftRun":
+				animated_sprite.play("LeftRun")
+				direction_facing = "Left"
+
+
+func _on_knockback_timeout() -> void:
+	is_knocked_back = false
